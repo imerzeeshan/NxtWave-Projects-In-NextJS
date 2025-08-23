ba8f6ac4d1b8145464543d6d1905a929c904af9f
Md Zeeshan <imerzeeshan@gmail.com>
2025-08-23 21:39:15 +0530

    Updating Functionality in app

---
+++ NxtTrendz/src/app/api/auth/product/route.ts
@@ -0,0 +1,57 @@
+import { connectToDatabase } from "@/lib/db";
+import Product from "@/models/Product";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    let { title, brand, price, rating, categoryType, imageUrl } = body;
+    console.log(title, brand, price, rating, categoryType, imageUrl);
+    if (!title || !brand || !price || !rating || !categoryType || !imageUrl) {
+      return NextResponse.json(
+        { success: false, message: "All Details are Required" },
+        { status: 400 }
+      );
+    }
+
+    // Convert string values to numbers if needed
+    price = Number(price);
+    rating = Number(rating);
+
+    // Validate price
+    if (isNaN(price) || price <= 0) {
+      return NextResponse.json(
+        { success: false, message: "Price must be a positive number" },
+        { status: 400 }
+      );
+    }
+
+    // Validate rating
+    if (isNaN(rating) || rating < 1 || rating > 5) {
+      return NextResponse.json(
+        { success: false, message: "Rating must be a number between 1 and 5" },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+    const product = await Product.create({
+      title,
+      brand,
+      price,
+      rating,
+      categoryType,
+      imageUrl,
+    });
+    return NextResponse.json(
+      { success: true, message: "Product Added Succesfully", product },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error adding product:", error);
+    return NextResponse.json(
+      { success: false, message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
