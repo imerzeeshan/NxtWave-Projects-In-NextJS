1a5cee15af2aa84922af1e2e38e3dcafdd5f48f9
Md Zeeshan <imerzeeshan@gmail.com>
2025-08-26 10:16:12 +0530

    Updating Functionality in app

--- NxtTrendz/src/app/api/auth/product/[id]/route.ts
+++ NxtTrendz/src/app/api/auth/product/[id]/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params;
